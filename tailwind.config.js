99ce40eb54c36e8b7604884e506d438be389299d
rafae2k <53544360+rafae2k@users.noreply.github.com>
2021-12-28 12:43:42 -0300

    setup: tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "primary-soft-blue": "hsl(215, 51%, 70%)",
+        "primary-cyan": "hsl(178, 100%, 50%)",
+        "main-BG": "hsl(217, 54%, 11%)",
+        "card BG": "hsl(216, 50%, 16%)",
+        "line-divider": "hsl(215, 32%, 27%)",
+      },
+      fontFamily: {
+        'sans': "Outfit, sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
